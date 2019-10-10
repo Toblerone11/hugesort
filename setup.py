7972ff94652538da23359c715a2562bac88e02b5
Ron Pick <ronpi1310@gmail.com>
2019-10-11 00:35:54 +0300

    add setup file to install as a library

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='hugesort',
+      version='1.0',
+      description='A tool for sorting huge lists (in terms of hundreds of millions)',
+      author='Ron Pick',
+      author_email='ronpi1310@gmail.com',
+      url='https://github.com/ronpik/hugesort',
+      py_modules=["hugesort"]
+      # packages=['distutils', 'distutils.command'],
+     )
